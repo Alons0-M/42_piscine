062898218b6add31a4195f5df7869f7de2378995
Alonso Marcos Arredondo Amador <aarredon@c1r5s3.42malaga.com>
2025-07-20 13:44:40 +0200

    c02 doing

---
+++ c02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarredon <aarredon@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 11:59:51 by aarredon          #+#    #+#             */
+/*   Updated: 2025/07/20 12:36:04 by aarredon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_alpha(char *str)
+{
+	int	alpha;
+
+	alpha = 1;
+	while (*str != '\0' && alpha)
+	{
+		if (!((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z')))
+			alpha = 0;
+		str++;
+	}
+	return (alpha);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d", ft_str_is_alpha(""));
+	printf("%d", ft_str_is_alpha("asff5"));
+	printf("%d", ft_str_is_alpha("5sdf"));
+	printf("%d", ft_str_is_alpha("5"));
+	printf("%d", ft_str_is_alpha("dasfsfereg"));
+}
---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarredon <aarredon@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 12:50:10 by aarredon          #+#    #+#             */
+/*   Updated: 2025/07/20 13:32:50 by aarredon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_alpha(char *str)
+{
+	int	alpha;
+
+	alpha = 1;
+	if (((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z')
+			|| (*str >= '0' && *str <= '9')))
+		alpha = 0;
+	return (alpha);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 || (ft_str_is_alpha(str + i - 1)))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[] = "salut, coMMent tu vas ? 42mots quaRAnte-deux; cinquante+et+un";
+	printf("%s \n", ft_strcapitalize(str));
+}
